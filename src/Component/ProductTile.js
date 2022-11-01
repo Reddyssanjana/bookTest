8380c717bf9168fa90e7abd2d199d93d135acd0a
Reddyssanjana <114550738+Reddyssanjana@users.noreply.github.com>
2022-11-01 15:53:41 +0530

    Add files via upload

--- src/Component/ProductTile.js
+++ src/Component/ProductTile.js
@@ -7,8 +7,8 @@ export const ProductTile = ({ product }) => {
     return (
 
         <Col>
-            <Card style={{ height: 200 }}>
-                {/* <Card.Img variant="top" style={{ height: 250 }} src={product.image} /> */}
+            <Card style={{ height: 500 }}>
+                 <Card.Img variant="top" style={{ height: 250 }} src={`https://covers.openlibrary.org/b/id/${product.cover_i}-L.jpg`} /> 
                 <Card.Header>Publish Date : {product.publish_date && product.publish_date.length > 0 && product.publish_date[0]} </Card.Header>
                 <Card.Body>
                     <Card.Title>{product.title} </Card.Title>
